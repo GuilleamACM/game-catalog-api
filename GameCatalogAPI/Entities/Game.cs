215a65ae7bf150422dd27bbda642a30c91de56e0
Guilherme Melo <gacm@cin.ufpe.br>
2021-09-25 11:56:05 -0300

    Adding the Game Entity and Repository

---
+++ GameCatalogAPI/Entities/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GameCatalogAPI.Entities
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+        
+        public string Title { get; set; }
+        
+        public string Developer { get; set; }
+        
+        public double Price { get; set; }
+    }
+}
---
+++ GameCatalogAPI/Repositories/IGameRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameCatalogAPI.Entities;
+
+namespace GameCatalogAPI.Repositories
+{
+    public interface IGameRepository
+    {
+        Task<List<Game>> Get(int page, int quantity);
+
+        Task<Game> Get(Guid id);
+
+        Task<Game> Get(string name, string developer);
+        
+        Task<Game> Insert(Game game);
+        
+        Task Update(Game game);
+
+        Task Remove(Guid id);
+    }
+}
