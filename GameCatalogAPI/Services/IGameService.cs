d7723afb605207f638df5cf25463f96b73977049
Guilherme Melo <gacm@cin.ufpe.br>
2021-09-25 11:17:33 -0300

    Adding IGameService interface

---
+++ GameCatalogAPI/Services/IGameService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameCatalogAPI.InputModel;
+using GameCatalogAPI.ViewModel;
+
+namespace GameCatalogAPI.Services
+{
+    public interface IGameService
+    {
+        Task<List<GameViewModel>> Get(int page, int quantity);
+
+        Task<GameViewModel> Get(Guid id);
+
+        Task<GameViewModel> Insert(GameInputModel game);
+
+        Task Update(Guid id, GameViewModel game);
+
+        Task Update(Guid id, double price);
+
+        Task Remove(Guid id);
+    }
+}
